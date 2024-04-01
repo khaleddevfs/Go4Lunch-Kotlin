ce6e65b735fc45a406c70df1daec045efcd00081
khaleddevfs <94543178+khaleddevfs@users.noreply.github.com>
2024-04-01 13:58:00 +0200

    kotlin

--- app/src/main/java/com/example/go4lunch24kotlin/models/Restaurant.kt
+++ app/src/main/java/com/example/go4lunch24kotlin/models/Restaurant.kt
@@ -3,6 +3,7 @@ package com.example.go4lunch24kotlin.models
 import com.example.go4lunch24kotlin.models.poko.Geometry
 import com.example.go4lunch24kotlin.models.poko.OpeningHours
 import com.example.go4lunch24kotlin.models.poko.Photo
+import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 /*
@@ -22,6 +23,44 @@ data class Restaurant constructor(
 )
 
 
+
+
+
+data class Restaurant(
+    @SerializedName("place_id")
+    var placeId: String? = null,
+
+    @SerializedName("name")
+    var restaurantName: String? = null,
+
+    @SerializedName("vicinity")
+    var restaurantAddress: String? = null,
+
+    @SerializedName("photos")
+    var restaurantPhotos: List<Photo>? = null,
+
+    @SerializedName("geometry")
+    var restaurantGeometry: Geometry? = null,
+
+    @SerializedName("opening_hours")
+    var openingHours: OpeningHours? = null,
+
+    @SerializedName("rating")
+    var rating: Double = 0.0,
+
+    @SerializedName("user_ratings_total")
+    var totalRatings: Int = 0,
+
+    @SerializedName("permanently_closed")
+    var isPermanentlyClosed: Boolean = false,
+
+    @SerializedName("formatted_phone_number")
+    var formattedPhoneNumber: String? = null,
+
+    @SerializedName("website")
+    var website: String? = null
+)
+ */
 
 data class Restaurant constructor(
     @SerializedName("place_id")
@@ -61,40 +100,4 @@ data class Restaurant constructor(
     @Expose
     var website: String? = null
 
-)
- */
-
-data class Restaurant(
-    @SerializedName("place_id")
-    var placeId: String? = null,
-
-    @SerializedName("name")
-    var restaurantName: String? = null,
-
-    @SerializedName("vicinity")
-    var restaurantAddress: String? = null,
-
-    @SerializedName("photos")
-    var restaurantPhotos: List<Photo>? = null,
-
-    @SerializedName("geometry")
-    var restaurantGeometry: Geometry? = null,
-
-    @SerializedName("opening_hours")
-    var openingHours: OpeningHours? = null,
-
-    @SerializedName("rating")
-    var rating: Double = 0.0,
-
-    @SerializedName("user_ratings_total")
-    var totalRatings: Int = 0,
-
-    @SerializedName("permanently_closed")
-    var isPermanentlyClosed: Boolean = false,
-
-    @SerializedName("formatted_phone_number")
-    var formattedPhoneNumber: String? = null,
-
-    @SerializedName("website")
-    var website: String? = null
 )
